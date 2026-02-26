e121e07a9a292f7c1bc20f383ab7a52655616cec
utxocrane <136141321+utxocrane@users.noreply.github.com>
2026-02-26 21:46:55 +0800

    Update t.js

--- t.js
+++ t.js
@@ -36,7 +36,7 @@ async function updateList() {
 	let allTxt = ''
 
 	//free-nodes/v2rayfree
-	try {
+	/*try {
     	const response = await axios.get('https://raw.githubusercontent.com/free-nodes/v2rayfree/main/README.md')
 		let txt = response.data
 		let si = txt.indexOf('```')+4,ei = txt.lastIndexOf('```')
@@ -44,7 +44,7 @@ async function updateList() {
 	  	
 	    console.log('订阅长度'+allTxt.length)
 	} catch (error) {console.error('free-nodes请求失败:', error.message);}
-
+	*/
 	/////////////////common base64 suburls
 	const suburls=[
 		'https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub',
@@ -68,8 +68,8 @@ async function updateList() {
 
 	//////////////包含节点URL的普通html(如vless|trojan://....)
 	const htmlPages=[
-		'https://github.com/Alvin9999-newpac/fanqiang/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7'
-		
+		'https://github.com/Alvin9999-newpac/fanqiang/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
+		'https://raw.githubusercontent.com/free-nodes/v2rayfree/main/README.md'
 	]
 
 	for(const u of htmlPages){
