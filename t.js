2181ddb9f011f74664ba90bf2c8c66e76f437309
utxocrane <136141321+utxocrane@users.noreply.github.com>
2026-02-27 11:51:28 +0800

    Update t.js

--- t.js
+++ t.js
@@ -7,6 +7,67 @@ let d = new Date()
 const yyyy=d.getFullYear(),mm = String(d.getMonth() + 1).padStart(2, '0'),dd= String(d.getDate()).padStart(2, '0');
 
 function fromBase64(bstr){return Buffer.from(bstr, 'base64').toString('utf-8')}
+function safeJson(s){return JSON.parse(s)}//暂代
+function fromBase64(bstr){return Buffer.from(bstr.trim(), 'base64').toString('utf-8')}
+function url2std(urlstr){//转为中间格式
+	let u = new URL(urlstr.replaceAll('&amp;','&'))
+
+	let ob = {protocol:u.protocol.slice(0,-1)}
+	
+	//vmess是json的base64编码，特殊情况单独处理
+	if(ob.protocol == 'vmess'){
+		let vj = safeJson(atob(urlstr.slice(8)))
+		
+		Object.assign(ob, {
+			hostname:vj.add,
+			port:vj.port,
+			username:vj.id,
+			scy:vj.scy||'auto',
+			aid:vj.aid ? parseInt(vj.aid):0,
+			hash:vj.ps,
+			net:vj.net,
+			path:vj.path,
+			host:vj.host
+		})
+	}else{//其他普通格式协议
+		[ob.hostname,ob.port,ob.hash,ob.username] = [u.hostname,u.port,decodeURI(u.hash.slice(1)),u.username];
+		for (let [k, v] of u.searchParams)
+			if(v&&v.length>0) ob[k] = v
+
+	}
+	
+	//修正数据格式、默认值等
+	ob.port = parseInt(ob.port)
+	try{ob.alpn = ob.alpn.split(',')}catch{}
+	if(ob.protocol == 'ss'){
+		ob.protocol = 'shadowsocks';
+		[ob.method,ob.password] = fromBase64(u.username).split(':');
+		//if(ob.method == 'chacha20-poly1305') ob.method = "chacha20-ietf-poly1305" //不支持？
+	}
+
+	return ob
+}
+
+function djb2Hash(str) {
+  let hash = 5381;
+  
+  for (let i = 0; i < str.length; i++) {
+    hash = ((hash << 5) + hash) + str.charCodeAt(i);
+  }
+  
+  hash = hash >>> 0; // 确保为正数
+  
+  // 转换为8位十六进制，不足补零
+  return hash.toString(16).padStart(8, '0');
+}
+const hashBy = ['protocol','hostname','port','username','sni','flow','pbk','type','sid','path','host','fp','net','serviceName','security','alpn']
+function urlHash(ob){//URL指纹
+	let str=""
+	for(p of hashBy) if(ob[p]) str+=JSON.stringify(ob[p])
+	
+	return djb2Hash(str)
+}
+
 
 function extractUrlsSmart(html) {
   const $ = cheerio.load(html);
@@ -37,8 +98,8 @@ async function updateList() {
 		`https://node.nodefree.me/${yyyy}/${mm}/${yyyy}${mm}${dd}.txt`,
 		'https://github.com/Alvin9999-newpac/fanqiang/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
 		'https://raw.githubusercontent.com/free-nodes/v2rayfree/main/README.md',
-		`https://node.hysteria2.org/uploads/${yyyy}/${mm}/0-${yyyy}${mm}${dd}.txt`,
-		`https://node.hysteria2.org/uploads/${yyyy}/${mm}/1-${yyyy}${mm}${dd}.txt`
+		`https://node.hysteria2.org/uploads/${yyyy}/${mm}/0-${yyyy}${mm}${dd}.txt`,`https://node.hysteria2.org/uploads/${yyyy}/${mm}/1-${yyyy}${mm}${dd}.txt`//https://hysteria2.org/free-node/2026-2-27-free-clash-subscribe.htm
+		
 		//https://raw.githubusercontent.com/adiwzx/freenode/main/adispeed.txt //这个自ID-10086/freenode导流的订阅已全部失效，后续再跟踪
 		//crossxx-labs/free-proxy是clash格式订阅，看看有无免费转换方案
 	]
@@ -72,6 +133,7 @@ async function updateList() {
 		catch (error) {console.error(u,'html解析节点链接失败:', error.message);}
 	}
 	*/
+	
 	
 	//fs.writeFileSync(path.join(process.cwd(),'v'), Buffer.from(allTxt.trim()).toString('base64'));
 	fs.writeFileSync('v', Buffer.from(allTxt.trim()).toString('base64'));
